43cbb6f18db15b5804d11ef67466fec24c660f18
rayan <randomhex1998@gmail.com>
2023-02-11 21:14:41 +0330

    add setting ico

--- src/components/TopSction/Topsection.jsx
+++ src/components/TopSction/Topsection.jsx
@@ -5,6 +5,7 @@ import Pic1 from "../../img/rayan.jpg";
 const Topsection = () => {
     return (
         <div className='top-section'>
+            <div className='setting-area'><i className='fa fa-cog'></i></div>
             <TopSectionContent ImgPic={Pic1}/>
         </div>
     );
