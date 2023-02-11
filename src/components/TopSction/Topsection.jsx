26ec61712db05b4d29332d08865a0f1f39de8e41
rayan <randomhex1998@gmail.com>
2023-02-11 19:18:15 +0330

    make top section component

---
+++ src/components/TopSction/Topsection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topsection = () => {
+    return (
+        <div className='top-section'>
+            555
+        </div>
+    );
+};
+
+export default Topsection;
