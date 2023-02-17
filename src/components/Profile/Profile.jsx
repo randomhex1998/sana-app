152ebbb786958295aee08506602b1f24183d37b9
rayan <randomhex1998@gmail.com>
2023-02-17 23:09:12 +0330

    make profile boxes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -15,12 +15,42 @@ const Profile = () => {
       </section>
       <section className="profile-opt-area">
         <section className="profile-opt-wrapper">
-          <ProfileOptions IcoName="fa-solid fa-store" OptionText="Shopping" />
-          <ProfileOptions IcoName="fa-solid fa-store" OptionText="Shopping" />
-          <ProfileOptions IcoName="fa-solid fa-store" OptionText="Shopping" />
-          <ProfileOptions IcoName="fa-solid fa-store" OptionText="Shopping" />
-          <ProfileOptions IcoName="fa-solid fa-store" OptionText="Shopping" />
-          <ProfileOptions IcoName="fa-solid fa-store" OptionText="Shopping" />
+          <ProfileOptions
+            IcoName="fa-solid fa-store"
+            OptionText="Shopping"
+            IcoBackground="rgba(26, 188, 156,0.2)"
+            IcoColor="#16a085"
+          />
+          <ProfileOptions
+            IcoName="fa-solid fa-taxi"
+            OptionText="Ubers"
+            IcoBackground="rgba(155, 89, 182,0.2)"
+            IcoColor="#8e44ad"
+          />
+          <ProfileOptions
+            IcoName="fa-solid fa-basket-shopping"
+            OptionText="Category"
+            IcoBackground="rgba(211, 84, 0,0.2)"
+            IcoColor="#d35400"
+          />
+          <ProfileOptions
+            IcoName="fa-solid fa-heart"
+            OptionText="Dates"
+            IcoBackground="rgba(231, 76, 60,0.2)"
+            IcoColor="#c0392b"
+          />
+          <ProfileOptions
+            IcoName="fa-solid fa-chart-column"
+            OptionText="State"
+            IcoBackground="rgba(241, 196, 15,0.2)"
+            IcoColor="#f39c12"
+          />
+          <ProfileOptions
+            IcoName="fa-brands fa-cc-paypal"
+            OptionText="Card"
+            IcoBackground="rgba(52, 152, 219,0.2)"
+            IcoColor="#2980b9"
+          />
         </section>
       </section>
     </>
