c37985754e15c4dcab510a195873c321e7b1ebc5
rayan <randomhex1998@gmail.com>
2023-02-17 19:15:35 +0330

    add icon to menu and make bg for it

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -3,21 +3,25 @@ export const Menu = () => {
     <ul className="menu-area">
       <li className="nav-items">
         <a href="#" className="nav-links">
+          <i class="fa-solid fa-house"></i>
           Home
         </a>
       </li>
       <li className="nav-items">
         <a href="#" className="nav-links">
+          <i class="fa-solid fa-newspaper"></i>
           News
         </a>
       </li>
       <li className="nav-items">
         <a href="#" className="nav-links">
+          <i class="fa-solid fa-cart-shopping"></i>
           Shop
         </a>
       </li>
       <li className="nav-items">
         <a href="#" className="nav-links">
+          <i class="fa-solid fa-user"></i>
           Profile
         </a>
       </li>
