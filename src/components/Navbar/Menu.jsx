2065569185344fb13cbc51284c824ac80170187c
rayan <randomhex1998@gmail.com>
2023-02-18 13:21:35 +0330

    make active link for menu section with react router dom

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -1,5 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export const Menu = () => {
+  const location = useLocation();
+  let [profileColor, setProfileColor] = useState(0);
+  let [newsColor, setNewsColor] = useState(0);
+  let [shopColor, setShopColor] = useState(0);
+  // let [profileColor, setProfileColor] = useState(0);
+
+  useEffect(() => {
+    if (location.pathname == "/profile") {
+      setProfileColor(1);
+    } else {
+      setProfileColor(0);
+    }
+    if (location.pathname == "/news") {
+      setNewsColor(1);
+    } else {
+      setNewsColor(0);
+    }
+    if (location.pathname == "/shop") {
+      setShopColor(1);
+    } else {
+      setShopColor(0);
+    }
+  }, [location]);
+
   return (
     <ul className="menu-area">
       <li className="nav-items">
@@ -9,19 +35,27 @@ export const Menu = () => {
         </a>
       </li>
       <li className="nav-items">
-        <a href="#" className="nav-links">
+        <Link
+          to="/news"
+          className={newsColor ? "color-menu-active nav-links" : "nav-links"}>
           <i className="fa-solid fa-newspaper"></i>
           News
-        </a>
+        </Link>
       </li>
       <li className="nav-items">
-        <a href="#" className="nav-links">
+        <Link
+          to="/shop"
+          className={shopColor ? "color-menu-active nav-links" : "nav-links"}>
           <i className="fa-solid fa-cart-shopping"></i>
           Shop
-        </a>
+        </Link>
       </li>
       <li className="nav-items">
-        <Link to="/profile" className="nav-links">
+        <Link
+          to="/profile"
+          className={
+            profileColor ? "color-menu-active nav-links" : "nav-links"
+          }>
           <i className="fa-solid fa-user"></i>
           Profile
         </Link>
